164cf7969e9c1d3da95ca455fdf7564f197600d4
ITP <RayhanUddinFarhad11@gmail.com>
2023-08-26 21:48:51 +0600

    user can update post now

--- src/components/dashboard/userDashboard/MyPosts.jsx
+++ src/components/dashboard/userDashboard/MyPosts.jsx
@@ -1,4 +1,5 @@
 'use client'
+import Modal from '@/components/modals/Modal';
 import { AuthContext } from '@/context/AuthProvider';
 import useMyPost from '@/hooks/useMyPost';
 import { useQuery } from '@tanstack/react-query';
@@ -7,14 +8,16 @@ import React, { useContext } from 'react';
 
 const MyPosts = () => {
 
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
-    
+
     const { isLoading, error, data: myposts = [], refetch } = useQuery({
         queryKey: ['myposts'],
         queryFn: () =>
             fetch(`http://localhost:5000/my-post/${user?.email}`).then((res) =>
                 res.json()
+
+            
             ),
     });
 
@@ -23,69 +26,101 @@ const MyPosts = () => {
     const handleDelete = (id) => {
 
         axios.delete(`http://localhost:5000/post/${id}`)
-        .then (res => {
-            console.log(res);
-            refetch()
-        })
-        .catch (err => {
-            console.log(err);
-        })
+            .then(res => {
+                console.log(res);
+                refetch()
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
 
     }
 
 
     return (
-        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-        <div className="items-start justify-between md:flex">
-            <div className="max-w-lg">
-                <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-                    Team members
-                </h3>
-                <p className="text-gray-600 mt-2">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                </p>
-            </div>
-          
-        </div>
-        <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
-            <table className="w-full table-auto text-sm text-left">
-                <thead className="bg-gray-50 text-gray-600 font-medium border-b">
-                    <tr>
-                        <th className="py-3 px-6">Photo</th>
-                        <th className="py-3 px-6">title</th>
-                        <th className="py-3 px-6">Category</th>
-                        <th className="py-3 px-6">status</th>
-                        <th className="py-3 px-6"></th>
+        <>
 
-                    </tr>
-                </thead>
-                <tbody className="text-gray-600 divide-y">
-                    {
-                        myposts?.map((item, idx) => (
-                            <tr key={idx}>
-                                <td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
-                                    <img src={item?.postImage} className="w-10 h-10 rounded-full" />
-                                    
-                                </td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item?.title}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item?.category}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item?.status}</td>
-                                <td className="text-right px-6 whitespace-nowrap">
-                                    <a  className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg">
-                                        Edit
-                                    </a>
-                                    <button onClick={() => handleDelete(item?._id)} className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
-                                        Delete
-                                    </button>
-                                </td>
+
+
+            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+                <div className="items-start justify-between md:flex">
+                    <div className="max-w-lg">
+                        <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
+                            Team members
+                        </h3>
+                        <p className="text-gray-600 mt-2">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </p>
+                    </div>
+
+                </div>
+                <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+                    <table className="w-full table-auto text-sm text-left">
+                        <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+                            <tr>
+                                <th className="py-3 px-6">Photo</th>
+                                <th className="py-3 px-6">title</th>
+                                <th className="py-3 px-6">Category</th>
+                                <th className="py-3 px-6">status</th>
+                                <th className="py-3 px-6"></th>
+
                             </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
-    </div>
+                        </thead>
+                        <tbody className="text-gray-600 divide-y">
+                            {
+                                myposts?.map((item, idx) => (
+                                    <>
+
+
+
+                                        <tr key={idx}>
+                                            <td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
+                                                <img src={item?.postImage} className="w-10 h-10 rounded-full" />
+
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">{item?.title}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap">{item?.category}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap">{item?.status}</td>
+                                            <td className="text-right px-6 whitespace-nowrap">
+
+
+
+
+                                                <label className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg" htmlFor="my_modal_6" >
+
+                                                    Edit
+                                                </label>
+
+                                                <button onClick={() => handleDelete(item?._id)} className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <Modal id={item?._id} title={item?.title}
+                                            description={item?.description}
+                                            category={item?.category}
+
+                                            postImage={item?.postImage}
+                                            refetch = {refetch}
+
+
+                                        ></Modal>
+
+                                    </>
+
+                                ))
+
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+
+
+        </>
     );
 };
 
---
+++ src/components/modals/Modal.jsx
@@ -0,0 +1,127 @@
+import { AuthContext } from '@/context/AuthProvider';
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+
+const Modal = ({ id, title, description, category, postImage, refetch }) => {
+    const { register, handleSubmit } = useForm(); // Initialize react-hook-form
+
+
+
+    const onSubmit = async (data) => {
+
+
+
+
+
+
+
+
+
+
+
+        axios.put(`http://localhost:5000/update-post/${id}`, {
+
+            title: data?.title,
+            description: data?.description,
+            category: data?.category,
+            photo: data?.photo,
+
+
+
+
+
+        })
+            .then(res => {
+                console.log(res);
+                refetch()
+            })
+            .catch(error => {
+                console.log(error);
+
+            })
+
+
+
+
+
+    };
+    return (
+        <>
+
+
+            <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box">
+                    <main className="w-full p-5 flex flex-col items-center justify-center bg-gray-50 sm:px-4">
+                        <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
+                            <div className="text-center">
+                                <div className="mt-5 space-y-2">
+                                    <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Update your post</h3>
+                                </div>
+                            </div>
+                            <div className="bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg">
+                                <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+                                    <div>
+                                        <label className="font-medium">Title</label>
+                                        <input
+                                            {...register('title')}
+                                            type="text"
+                                            defaultValue={title}
+                                            placeholder='What is web development?'
+                                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="font-medium">Description</label>
+                                        <textarea
+                                            defaultValue={description}
+                                            {...register('description')}
+                                            type="text"
+                                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border border-indigo-600 shadow-sm rounded-lg textarea h-36"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="font-medium">Category</label>
+                                        <select
+                                            defaultValue={category}
+                                            {...register('category')}
+                                            className="select select-bordered w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                        >
+                                            <option selected disabled>Select a category</option>
+                                            <option value="Web Development">Web Development</option>
+                                            <option value="Programming Career">Programming Career</option>
+                                            <option value="Machine Learning">Machine Learning</option>
+                                            <option value="Programming Language">Programming Language</option>
+                                            <option value="Motivational">Motivational</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label className="font-medium">Upload photo</label>
+                                        <input
+                                            {...register('photo')}
+                                            type="text"
+                                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                            placeholder='photo URL'
+                                        />
+                                    </div>
+                                    <button
+                                        type="submit"
+                                        className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                                    >
+                                        Post
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </main>
+
+                    <div className="modal-action">
+                        <label htmlFor="my_modal_6" className="btn">Close!</label>
+                    </div>
+                </div>
+            </div></>
+    );
+};
+
+export default Modal;
